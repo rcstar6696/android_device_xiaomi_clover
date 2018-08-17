18c80b1a337cac7daa808e2498349c59876df3b0
guaiyihu <guaiyihu@foxmail.com>
2018-08-17 21:45:34 +0800

    [DNM] Temp drop ANT
    Change-Id: Ic35fb3f14ea61509a251ec2b0fbb11c6a341844c

--- device.mk
+++ device.mk
@@ -132,12 +132,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
 
 # ANT+
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \
     libantradio
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
