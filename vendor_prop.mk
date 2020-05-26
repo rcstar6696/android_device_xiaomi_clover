896c8a81dd2697b96e6d0a4b74da77285031a84b
Max Weffers <rcstar6696@gmail.com>
2020-05-26 14:32:59 +0200

    clover: Import Mi Pad 4 Audio props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,3 +1,7 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.audiorec=true
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.stats.test=5 \
@@ -8,3 +12,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.vds.allow_hwc=0
 
+# Ultra Sound
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.chk.cal.us=0
